0d87b0fa6ec5ac1a39f69735a2ff9053cefa2dbb
flowerrhythmrmx <flowerrhythmremix@gmail.com>
2021-01-29 02:55:52 +0000

    Added crypto search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+import requests
+import json
 import os
 import random
 
@@ -22,4 +24,20 @@ async def on_message(message):
       result = random.choice(choices)
       await message.channel.send(result)
 
+    if message.content.startswith('%'):
+      ticker = message.content[1:].upper()
+      url = "https://min-api.cryptocompare.com/data/price"
+      headers = {
+        'Content-type': 'application/json',
+        'Authorization': os.getenv('CRYPTOCOMPARE_KEY')
+      }
+      payload = { 'fsym': ticker, 'tsyms': 'USD' }
+
+      request = requests.get(url, params=payload, headers=headers)
+
+      if ticker == 'DOGE':
+        ticker = ':doge:'
+
+      await message.channel.send("CRYPTO: " + ticker + " is currently trading at $" + str(request.json()['USD']))
+      
 client.run(os.getenv('TOKEN'))
